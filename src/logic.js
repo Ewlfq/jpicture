89c2b91faa7ee35484d3d9f28b5c6b483ad0a033
oliver-j <jessnerol@stud.sbg.ac.at>
2015-06-14 20:48:59 +0200

    width test

--- src/logic.js
+++ src/logic.js
@@ -1,6 +1,13 @@
 $( document ).ready(function() {
+    console.log('Parameter test');
     $('#first').jp('String');
     $('#second').jp(13);
     $('#third').jp([1,2,3]);
+    $('#fourth').jp({ 'a' : 160});
+    
+    console.log('Width test');
+    $('#first').jp({ 'a' : 160});
+    $('#second').jp({ 'a' : 160});
+    $('#third').jp({ 'a' : 160};
     $('#fourth').jp({ 'a' : 160});
 });

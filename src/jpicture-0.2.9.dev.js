18122a31e0f05442aacbe10e13b72fa11d592ca6
oliver-j <jessnerol@stud.sbg.ac.at>
2015-06-22 15:44:53 +0200

    new minified version

--- src/jpicture-0.2.9.dev.js
+++ src/jpicture-0.2.9.dev.js
@@ -10,7 +10,7 @@
          
     return this.each(function () {
                 
-        // this obj stores all css properties
+        // this obj stores all css properties.
         var containerCSS = { 
             'background-repeat' : 'no-repeat',
             'background-size' : 'cover',
@@ -18,12 +18,12 @@
             orientationChange : true
         },
         
-        // this obj stores all properties
+        // this obj stores all properties.
         containerProp = {
             callback : null,
             animationSpeed: 'fast',
             lastPicture : {
-                height : 0, // at the moment not needed 
+                height : 0, // at the moment not needed.
                 width : 0
             } 
         },
@@ -56,13 +56,13 @@
             };
         },
     
-        // this calucates the growing or shrink height of the element 
+        // this calucates the growing or shrink height of the element.
         // otherwise it would take height of the picture.
         calcResizingRatio = function (picWidth, containerWidth, height) { 
             return height * (1 + ((containerWidth - picWidth) / picWidth)); 
         },
         
-        // a little animation to, make the loading smoother
+        // a little animation to, make the loading smoother.
         animatePicutreLoad = function (container, fun) {
             $(container).fadeOut(containerProp.animationSpeed, function () {
                 fun();
@@ -70,8 +70,8 @@
             });
         },
         
-        // This function fetches the image through ajax. In case of a non-img
-        // element it also checks if the height is 0. If it is 0 it resizes the 
+        // This function fetches the image through ajax. In case of a non-img.
+        // element it also checks if the height is 0. If it is 0 it resizes the.
         // non-img element to the height of the picture.
         fetchImg =  function (container, imgProp, containerWidth) {  
             var useImg = function (imgTag, url) {
@@ -82,7 +82,7 @@
             }, 
             // container is the non-img tag.
             useDiv = function (container, url) {
-                var loadImg = new Image(); // We need this image obj to get the height 
+                var loadImg = new Image(); // We need this image obj to get the height. 
  
                 // it is also very important to check dynamic heights, like navi pictures,
                 // those get a predefined height of their inside elements example: oliverj.net.
@@ -149,34 +149,34 @@
         }, 
     
         initParameters = function (container, picList, p1, p2) {            
-            // First optional parameter is a callback
+            // First optional parameter is a callback.
             if (checkType(p1, 'Function')) {
                 containerProp.callback = p1; 
             } 
             
-            // First optional parameter is a object
+            // First optional parameter is a object.
             if (checkType(p1, 'Object')) {      
                 containerCSS = $.extend( {}, containerCSS, p1);
                 
-                // First optional parameter is object and the second one is a callback
+                // First optional parameter is object and the second one is a callback.
                 if (checkType(p2, 'Function')) {
                     containerProp.callback = p2; 
                 }
             }   
-            // optional parameter for enabling/disabling Zoom, default on
+            // optional parameter for enabling/disabling Zoom, default on.
             if (containerCSS.enableZoom) {
                 onZoom(container, picList);
             }
-            // optional parameter for orientationChange, default on
+            // optional parameter for orientationChange, default on.
             if (containerCSS.orientationChange) {
                 win.addEventListener('orientationchange', onOrientationChange(container, picList));
             }
             
-            delete containerCSS.enableZoom; // not very happy about this
-            delete containerCSS.orientationChange; // not very happy about this
+            delete containerCSS.enableZoom; // not very happy about this.
+            delete containerCSS.orientationChange; // not very happy about this.
         };
         
-        // Plugin starts here
+        // Plugin starts here.
         initParameters(this, picList, p1, p2);
         setPicture(this, picList); 
         
